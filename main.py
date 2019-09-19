8d25d27eb856844c085e9e7720801b2bfba636ec
hassan <hassantarek201517@gmail.com>
2019-09-20 01:10:24 +0200

    initial probability

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from pandas.core.groupby import GroupBy
+
+
+def is_string(dataframe, element):
+    column = dataframe[element]
+    c = np.array(column)
+    tmp = c[0]
+    return isinstance(tmp, str)
+
+
+def prob(dataframe, col):
+    group = dataframe.groupby(col)
+    num_group = group.size()
+    return num_group / len(dataframe)
+
 
 data = pd.read_csv('cell2celltrain.csv')
-data.dropna(how='any',inplace=True)
+data.dropna(how='any', inplace=True)
 columns = list(data)
-print(data)
+total = len(data)
+prob_df_yn = pd.DataFrame()
+prob_df_str = pd.DataFrame()
+keys = []
+frames = []
+for col in columns:
+    if (is_string(data, col) and
+            (str(data[col][0]) == "Yes" or str(data[col][0]) == "No")):
+        prob_col = (prob(data, col))
+        prob_df_yn[col] = prob_col
+    elif is_string(data, col):
+        keys.append(col)
+        tmp = pd.DataFrame(prob(data, col))
+        frames.append(tmp)
+prob_df_str = pd.concat(frames, keys=keys)
